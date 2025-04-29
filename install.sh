1a130f737a65a29523235c8bb71790ba710d327b
gao5yy <125730758+gao5yy@users.noreply.github.com>
2025-04-29 10:23:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 echo "****************** Installing pytorch ******************"
-conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=10.1 -c pytorch
+conda install pytorch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 cudatoolkit=11.3 -c pytorch
 
 echo ""
 echo ""
